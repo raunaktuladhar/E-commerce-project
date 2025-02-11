76ed02a4417d95dbaac1d19379176b6288ba07f2
Raunak Tuladhar <52153091+raunaktuladhar@users.noreply.github.com>
2025-02-11 09:05:19 +0545

    Update Cart.jsx

--- Frontend/create-react-app/src/Pages/Cart.jsx
+++ Frontend/create-react-app/src/Pages/Cart.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import CartItems from '../Components/CartItems/CartItems.jsx';
+import KhaltiPayment from "../Components/KhaltiPayment/KhaltiPayment.jsx";
 
 const Cart = () => {
     return(
         <div>
-            <CartItems></CartItems>
+            <CartItems>
+                <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+                    <KhaltiPayment></KhaltiPayment>
+                </div>
+            </CartItems>
         </div>
     )
 }
